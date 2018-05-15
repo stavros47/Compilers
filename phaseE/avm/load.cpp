0706316375e7225b1469a4ee23c91530bbde5d60
Nikolas <csd3444@csd.uoc.gr>
2018-05-15 15:08:10 +0300

    load

--- phaseE/avm/load.cpp
+++ phaseE/avm/load.cpp
@@ -1,7 +1,28 @@
 #include "headers/avm.h"
+#include <assert.h>
+#include <fstream>
 
+int main(int argc, char* argv[]){ // or char** argv 
+	if(argc<2) assert(false);
 
+	std::ifstream infile(argv[1],std::fstream::in);
+	std::string output;
 
-int main(){
+	if(infile.is_open()){
+		infile>>output;
+		if(output.compare("474747")){
+			std::cout<<"Not an abc file\n";
+			return -1;
+		}
+
+		while(!infile.eof()){
+			infile>>output;
+
+			std::cout<<output<<"\n";				
+		}
+	}
+
+	infile.close();
+	
 	return 0;
 }
