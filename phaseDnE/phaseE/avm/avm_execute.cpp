128085ab9f0352315064a1a3769b5d514b3a5f90
SArgyrou <stavris.47@gmail.com>
2018-05-19 15:27:35 +0300

    Dispachers & executes for jump Leq - Geq - Lt - Gt

--- phaseDnE/phaseE/avm/avm_execute.cpp
+++ phaseDnE/phaseE/avm/avm_execute.cpp
@@ -87,7 +87,7 @@ void execute_call( instruction* instr){
 void execute_not(instruction* instr){}////////////////////////////////////////
 
 unsigned char jump_checks(avm_memcell* rv1,avm_memcell* rv2){
-        if(rv1->type == undef_m || rv2->type ==undef_m){
+        if(rv1->type == undef_m || rv2->type == undef_m){
                 avm_error("'undef' involved in equality!");
         }else
         if(rv1->type == bool_m || rv2->type == bool_m){
@@ -102,9 +102,59 @@ unsigned char jump_checks(avm_memcell* rv1,avm_memcell* rv2){
                 typeStrings[rv2->type]);
         }else{
                 check_eq_func_t f = check_eqFuncs[rv1->type];
-                if(f)
+                if(f){
                         return (*f)(rv1,rv2);
-                else assert(0);
+                }else {
+                        assert(0);
+                }
+        }
+}
+
+unsigned char jump_leChecks(avm_memcell* rv1,avm_memcell* rv2){ //??
+        if(rv1->type == undef_m || rv2->type == undef_m){
+                avm_error("'undef' involved in equality!");
+        }else
+        if(rv1->type == bool_m || rv2->type == bool_m){
+                return (avm_tobool(rv1) <= avm_tobool(rv2));
+        }else
+        if(rv1->type == nil_m || rv2->type == nil_m){
+                return (avm_tobool(rv1) <= avm_tobool(rv2));
+        }else
+        if(rv1->type != rv2->type){
+        avm_error("%s<=%s is illegal!",
+                typeStrings[rv1->type],
+                typeStrings[rv2->type]);
+        }else{
+                check_eq_func_t f = check_leFuncs[rv1->type];
+                if(f){
+                        return (*f)(rv1,rv2);
+                }else {
+                        assert(0);
+                }
+        }
+}
+
+unsigned char jump_geChecks(avm_memcell* rv1,avm_memcell* rv2){ //??
+        if(rv1->type == undef_m || rv2->type == undef_m){
+                avm_error("'undef' involved in equality!");
+        }else
+        if(rv1->type == bool_m || rv2->type == bool_m){
+                return (avm_tobool(rv1) >= avm_tobool(rv2));
+        }else
+        if(rv1->type == nil_m || rv2->type == nil_m){
+                return (avm_tobool(rv1) >= avm_tobool(rv2));
+        }else
+        if(rv1->type != rv2->type){
+        avm_error("%s>=%s is illegal!",
+                typeStrings[rv1->type],
+                typeStrings[rv2->type]);
+        }else{
+                check_eq_func_t f = check_geFuncs[rv1->type];
+                if(f){
+                        return (*f)(rv1,rv2);
+                }else {
+                        assert(0);
+                }
         }
 }
 
@@ -132,10 +182,54 @@ void execute_jne(instruction* instr){
         if(!executionFinished && !result)
                 pc = instr->result.val;
 }
-void execute_jle(instruction* instr){}
-void execute_jge(instruction* instr){}
-void execute_jlt(instruction* instr){}
-void execute_jgt(instruction* instr){}
+void execute_jle(instruction* instr){
+        assert(instr->result.type == label_a);
+        avm_memcell* rv1 = avm_translate_operand(&instr->arg1,&ax);
+        avm_memcell* rv2 = avm_translate_operand(&instr->arg2,&bx);
+        unsigned char result = 0;
+
+        result = jump_leChecks(rv1,rv2);
+                
+        if(!executionFinished && result)
+                pc = instr->result.val;
+
+}
+void execute_jgt(instruction* instr){
+        assert(instr->result.type == label_a);
+        avm_memcell* rv1 = avm_translate_operand(&instr->arg1,&ax);
+        avm_memcell* rv2 = avm_translate_operand(&instr->arg2,&bx);
+        unsigned char result = 0;
+
+        result = jump_leChecks(rv1,rv2);
+                
+        if(!executionFinished && !result)
+                pc = instr->result.val;
+
+}
+void execute_jge(instruction* instr){
+        assert(instr->result.type == label_a);
+        avm_memcell* rv1 = avm_translate_operand(&instr->arg1,&ax);
+        avm_memcell* rv2 = avm_translate_operand(&instr->arg2,&bx);
+        unsigned char result = 0;
+
+        result = jump_geChecks(rv1,rv2);
+                
+        if(!executionFinished && result)
+                pc = instr->result.val;
+}
+void execute_jlt(instruction* instr){
+        assert(instr->result.type == label_a);
+        avm_memcell* rv1 = avm_translate_operand(&instr->arg1,&ax);
+        avm_memcell* rv2 = avm_translate_operand(&instr->arg2,&bx);
+        unsigned char result = 0;
+
+        result = jump_geChecks(rv1,rv2);
+                
+        if(!executionFinished && !result)
+                pc = instr->result.val;
+}
+
+
 void execute_pusharg(instruction* instr){
         avm_memcell* arg = avm_translate_operand(&instr->result,&ax);
         assert(arg);
